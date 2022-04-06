87a437153c28a9a736394259ca7d445f65f2c2b5
paleksanderr <pawel.aleksander@gmail.com>
2022-04-06 11:53:05 +0200

    background generator

--- newpr/app.js
+++ newpr/app.js
@@ -1,25 +1,19 @@
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
 
-let button = document.querySelector('#enter')
-let input = document.querySelector('#userInput');
 
-function inputLength() {
-  return input.value.length    
-}
-function createListElement() {
-  { 
-  let li = document.createElement('li');
-  li.appendChild(document.createTextNode(input.value));
-  document.querySelector('ul').appendChild(li);}
-  input.value = '';
-}
 
-function addListByClick() {
-  if (inputLength() > 0) createListElement();
-}
-function addListByKeypress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) 
-    createListElement();
-  }
-button.addEventListener('click', addListByClick);
-input.addEventListener("keypress", addListByKeypress );
+console.log(css);
+console.log(color1.value);
+console.log(color2);
+
+function setGradient() {
+ body.style.background =
+   "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";  
+};
+
+color1.addEventListener("input", setGradient); 
+color2.addEventListener("input", setGradient);
